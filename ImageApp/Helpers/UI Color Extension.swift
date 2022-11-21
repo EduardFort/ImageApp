ef1cd7c16b3e6d723158bcb2f5a006d638360596
eduardfort <eduardseleznyov@gmail.com>
2022-11-21 11:33:10 +0300

    исправлены горизонт. отступы ячейки

---
+++ ImageApp/Helpers/UI Color Extension.swift
@@ -0,0 +1,14 @@
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "Black")! }
+    static var ypWhite: UIColor { UIColor(named: "White")! }
+    static var ypBlue: UIColor { UIColor(named: "Blue")! }
+    static var ypRed: UIColor   { UIColor(named: "Red")! }
+    static var ypGray: UIColor  { UIColor(named: "Gray")! }
+    static var ypBackGround: UIColor { UIColor(named: "Background")! }
+    static var ypWhite50: UIColor { UIColor(named: "White50")! }
+}
+
+
---
+++ ImagesList/ImagesListViewController.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    private var photosName = [String]()
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        photosName = Array(0..<20).map{ "\($0)" }
+    }
+    
+}
+
+extension ImagesListViewController:UITableViewDelegate {}
+
+extension ImagesListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photosName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+            
+            guard let imageListCell = cell as? ImagesListCell else {
+                return UITableViewCell()
+            }
+            
+        configCell(for: imageListCell, with: indexPath)
+            return imageListCell
+        }
+}
+
+extension ImagesListViewController {
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+        }
+        
+        guard let gradient = UIImage(named: "cellGradient") else {
+            return
+        }
+        
+        cell.gradient.image = gradient
+
+        cell.cellImage.image = image
+        cell.cellDate.text = dateFormatter.string(from: Date())
+
+        let isLiked = indexPath.row % 2 == 0
+        let likeImage = isLiked ? UIImage(named: "likeButtonOff") : UIImage(named: "likeButtonOn")
+        cell.likeButton.setImage(likeImage, for: .normal)
+    }
+}
